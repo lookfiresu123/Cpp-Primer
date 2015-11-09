96ed57c69f556045d9dd96b8ba32dde0e172e60b
lookfiresu123 <lookfiresu123@gmail.com>
2015-11-09 22:02:00 +0800

    finish chapter_1

---
+++ ch01/practice/Sales_item.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Sales_item.h"
+
+int main (void) {
+    // Sales_item book;
+    // std::cin >> book;
+    // std::cout << book << std::endl;
+
+    Sales_item item1, item2;
+    std::cin >> item1 >> item2;
+    std::cout << item1 + item2 << std::endl;
+    return 0;
+}
---
+++ ch01/practice/test_8.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main (void) {
+    std::cout << /* "*/" /* "/*" */;
+    //std::cout << /* "*/" */;
+    return 0;
+}
