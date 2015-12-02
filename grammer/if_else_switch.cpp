d55ba67f0497528e714ec283a78c4e20c7b8239f
lookfiresu123 <lookfiresu123@gmail.com>
2015-12-02 15:48:44 +0800

    modify loop and if-else file

--- grammer/if_else_switch.cpp
+++ grammer/if_else_switch.cpp
@@ -6,9 +6,13 @@ using namespace std;
 
 #define DO_TEST_IF_ELSE_1 0
 #define DO_TEST_IF_ELSE_2 0
-#define DO_TEST_IF_ELSE_3 1
+#define DO_TEST_IF_ELSE_3 0
 #define DO_TEST_IF_ELSE_4 0
 #define DO_TEST_IF_ELSE_5 0
+#define DO_TEST_IF_ELSE_6 0
+#define DO_TEST_IF_ELSE_7 0
+#define DO_TEST_IF_ELSE_8 1
+#define DO_TEST_IF_ELSE_9 0
 
 int test_if_else_1 (void) {
     int grade = 10;
@@ -60,6 +64,137 @@ int test_if_else_3 (void) {
     return 0;
 }
 
+int test_if_else_4 (void) {
+    int ch = 'a';
+    int cnt1 = 0, cnt2 = 0;
+    switch (ch) {
+        case 'a':
+        case 'b':
+        case 'c':
+            ++cnt1;
+            break;
+        case 'd':
+        case 'e':
+        case 'f':
+            ++cnt2;
+            break;
+    }
+    return 0;
+}
+
+/* 练习5.9 */
+int test_if_else_5 (void) {
+    char ch;
+    int vowelcnt = 0, othercnt = 0;
+    while (cin >> ch) {
+        switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                ++vowelcnt;
+                break;
+            default:
+                ++othercnt;
+                break;
+        }
+    }
+    cout << "there are "<< vowelcnt << " vowel letter" << endl;
+    return 0;
+}
+
+/* 练习5.10 */
+int test_if_else_6 (void) {
+    char ch;
+    int vowelcnt = 0, othercnt = 0;
+    while (cin >> ch) {
+        switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                ++vowelcnt;
+                break;
+            default:
+                ++othercnt;
+                break;
+        }
+    }
+    cout << "there are "<< vowelcnt << " vowel letter" << endl;
+    return 0;
+}
+
+/* 练习5.11 */
+int test_if_else_7 (void) {
+    char ch;
+    int vowelcnt = 0, spacecnt = 0, tabcnt = 0, newlinecnt = 0;
+    int othercnt = 0;
+    while (cin >> ch) {
+        switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                ++vowelcnt;
+                break;
+            case ' ':
+                ++spacecnt;
+                break;
+            case '\t':
+                ++tabcnt;
+                break;
+            case '\n':
+                ++newlinecnt;
+                break;
+            default:
+                ++othercnt;
+                break;
+        }
+    }
+    return 0;
+}
+
+/* 练习5.12 */
+int test_if_else_8 (void) {
+    char ch, prech = '\0';
+    int ffcnt = 0, ficnt = 0, flcnt = 0;
+    while (cin >> ch) {
+        switch (ch) {
+            case 'f':
+                if (prech == 'f')
+                    ++ffcnt;
+                break;
+            case 'i':
+                if (prech == 'f')
+                    ++ficnt;
+                break;
+            case 'l':
+                if (prech == 'f')
+                    ++flcnt;
+                break;
+        }
+        prech = ch;
+    }
+    cout << "ff = " << ffcnt << endl;
+    cout << "fi = " << ficnt << endl;
+    cout << "fl = " << flcnt << endl;
+    return 0;
+}
+
+
 int main (void) {
 #if DO_TEST_IF_ELSE_1
     test_if_else_1();
@@ -75,6 +210,21 @@ int main (void) {
 #endif
 #if DO_TEST_IF_ELSE_5
     test_if_else_5();
+#endif
+#if DO_TEST_IF_ELSE_6
+    test_if_else_6();
+#endif
+#if DO_TEST_IF_ELSE_7
+    test_if_else_7();
+#endif
+#if DO_TEST_IF_ELSE_8
+    test_if_else_8();
+#endif
+#if DO_TEST_IF_ELSE_9
+    test_if_else_9();
+#endif
+#if DO_TEST_IF_ELSE_10
+    test_if_else_10();
 #endif
 
 
--- grammer/loop.cpp
+++ grammer/loop.cpp
@@ -7,8 +7,8 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-#define DO_TEST_WHILE_1 1
-#define DO_TEST_WHILE_2 0
+#define DO_TEST_WHILE_1 0
+#define DO_TEST_WHILE_2 1
 
 /* for while */
 
@@ -18,6 +18,26 @@ int test_while_1 (void) {
     string::iterator iter = s.begin();
     while (iter != s.end())
         cout << *iter++ << endl;
+    return 0;
+}
+
+// 练习5.14
+int test_while_2 (void) {
+    string temp, max_string;
+    string prev;  // 保持前一个string对象
+    int max_occur = 0, cur_occur = 0;
+    while (cin >> temp) {
+        if (temp == prev)
+            ++cur_occur;
+        else {
+            if (cur_occur > max_occur) {
+                max_occur = cur_occur;
+                max_string = prev;
+            }
+            cur_occur = 1;
+        }
+        prev = temp;
+    }
     return 0;
 }
 
