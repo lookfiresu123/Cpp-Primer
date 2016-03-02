0e17242219b2b39549f8596991ee485ce24842b2
lookfiresu123 <lookfiresu123@gmail.com>
2016-03-02 16:00:57 +0800

    finish to 13.3

--- practice/chapter_13/test.cpp
+++ practice/chapter_13/test.cpp
@@ -1,11 +1,73 @@
 #include "test.h"
 
-void fcn(const Test *p1, Test p2) {
-    Test t1(*p1), t2(p2);
+void fcn(const Test *q1, Test q2) {
+    Test t1(*q1), t2(q2);
+}
+
+void f(numbered s) {
+    s.print();
+}
+
+int Employee::id = 0;
+
+HasPtr &HasPtr::operator=(const HasPtr &that) {
+    if (!(*ps).empty())
+        delete ps;
+    ps = new string(*that.ps);
+    i = that.i;
+    cout << "HasPtr &operator=(const HasPtr &)" << endl;
+    return *this;
+}
+
+HasPtrShare &HasPtrShare::operator=(const HasPtrShare &that) {
+    ++*that.use;
+    if (--*use == 0) {
+        delete use;
+        delete ps;
+    }
+    ps = that.ps;
+    i = that.i;
+    use = that.use;
+    return *this;
+}
+
+HasPtrShare::~HasPtrShare() {
+    if (--*use == 0) {
+        delete use;
+        delete ps;
+    }
 }
 
 int main() {
+#if TEST_CONSTRUCTOR_ORDER
     Test p1, p2;
     fcn(&p1, p2);
+#endif
+#if TEST_NUMBERED
+    numbered a, b = a, c = b;
+    f(a); f(b); f(c);
+#endif
+#if TEST_PRACTICE_1
+    Employee p1;
+    p1.print();
+    Employee p2("chensu");
+    p2.print();
+#endif
+#if TEST_CLASS_VALUE_USAGE
+    HasPtr p1("chensu");    // 直接构造函数
+    HasPtr p2(p1);      // 拷贝构造函数
+    HasPtr p3 = p2;     // 拷贝构造函数
+    p1 = p2;        // 赋值
+#endif
+#if TEST_CLASS_POINTER_USAGE
+    HasPtrShare p1("chensu");    // 直接构造函数
+    p1.addi(); p1.print();
+    HasPtrShare p2(p1);      // 拷贝构造函数
+    p2.addi(); p2.print();
+    HasPtrShare p3 = p2;     // 拷贝构造函数
+    p3.addi(); p3.print();
+    p1 = p2;        // 赋值
+    p1.addi(); p1.print();
+#endif
     return 0;
 }
--- practice/chapter_13/test.h
+++ practice/chapter_13/test.h
@@ -2,14 +2,74 @@
 #define _H_TEST
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+#define TEST_CONSTRUCTOR_ORDER 0
+#define TEST_NUMBERED 0
+#define TEST_PRACTICE_1 0
+#define TEST_CLASS_VALUE_USAGE 0
+#define TEST_CLASS_POINTER_USAGE 0
+#define TEST_PRACTICE_2 1
+#define TEST_PRACTICE_3 0
+#define TEST_PRACTICE_4 0
+#define TEST_PRACTICE_5 0
+#define TEST_PRACTICE_6 0
 
 using namespace std;
 
 class Test {
 public:
-    Test() { cout << this << " -> allocate memory " << endl; }
-    Test(const Test &) { cout << this << " -> allocate memory " << endl; }
-    ~Test() { cout << this << " -> free memory" << endl; }
+    Test() { cout << this << " : Test()" << endl; }
+    Test(const Test &) { cout << this << " : Test(const Test &)" << endl; }
+    ~Test() { cout << this << " : ~Test()" << endl; }
+};
+
+class numbered {
+public:
+    numbered() { srand(time(0)); mysn = rand() % 1000; }
+    numbered(const numbered &p) { mysn = p.mysn; srand(time(0)); mysn = rand() % 1000; }
+    void print() { cout << "mysn = " << mysn << endl; }
+private:
+    int mysn;
+};
+
+class Employee {
+public:
+    Employee() { ++id; }
+    Employee(const string &_name) : name(_name) { ++id; }
+    ~Employee() {}
+    void print() { cout << "name = " << name << ", " << "id = " << id << endl; }
+private:
+    string name;
+    static int id;
+};
+
+class HasPtr {
+public:
+    HasPtr(const string &s) : ps(new string(s)), i(0) { cout << "HasPtr(const string &s)" << endl; }
+    HasPtr(const HasPtr &p) : ps(new string(*p.ps)), i(p.i) { cout << "HasPtr(const HasPtr &p)" << endl; }
+    HasPtr &operator=(const HasPtr &);
+    ~HasPtr() { delete ps; }
+private:
+    string *ps;
+    int i;
+};
+
+class HasPtrShare {
+public:
+    // direct constructor allocate new string and new calculator which set to 1
+    HasPtrShare(const string &s) : ps(new string(s)), i(0), use(new size_t(1)) {}
+    // copy constructor set string and set calculator pointer to the same one, and then add 1 to it
+    HasPtrShare(const HasPtrShare &p) : ps(p.ps), i(p.i), use(p.use) { ++*use; }
+    HasPtrShare &operator=(const HasPtrShare &);
+    ~HasPtrShare();
+    void print() { cout << *ps << ", " << i << ", " << *use << endl;}
+    void addi() { ++i; }
+private:
+    string *ps;
+    int i;
+    size_t *use;   // use count
 };
 
 #endif
